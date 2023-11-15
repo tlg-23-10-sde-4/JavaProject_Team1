8610e83aa2dae3651f26cdaf87f97586f223beb7
John Pate <john.pate@tlgcohort.com>
2023-11-15 14:49:49 -0600

    Animal class first draft

--- src/com/typehunter/Animal.java
+++ src/com/typehunter/Animal.java
@@ -1,4 +1,39 @@
 package com.typehunter;
 
+import java.util.ArrayList;
+
 class Animal {
+    private String name;
+    private int size;
+    private int[] wordCount;
+    private Collection<Word> words = new ArrayList<>();
+    // sound
+    // location
+
+    public Animal (String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public void appear() {
+
+    }
+
+    public void flee () {
+
+    }
+
+    public void confirmHit() {
+
+    }
+
+    public void wordBank() {
+
+    }
+
+    public void wordCount() {
+
+    }
+
+
 }
