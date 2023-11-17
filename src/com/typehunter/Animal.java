056920d36b225b803eb48e6e3ea10733a8d817e8
John Pate <john.pate@tlgcohort.com>
2023-11-17 14:15:39 -0600

    Animal class seventh draft

--- src/com/typehunter/Animal.java
+++ src/com/typehunter/Animal.java
@@ -8,26 +8,46 @@ import java.io.IOException;
 import java.util.*;
 
 public class Animal<Word> {
+    private static String animalConstructor ="animalConstructor.txt";
     private final String name;
-    private  String imageFile = "filePath";   // pulls from file/path
-    private  String soundFile = "filePath";   // pulls from file/path
+    private String imageFile;   // pulls from file/path
+    private String soundFile;   // pulls from file/path
     private String wordCount;
     //private final List<String> words = new ArrayList<>();
     // NOTE1: Do we need int size for number or words per animal or String[] with a size of... Line 17-18
     private String[] wordsArray = new String[0];
-    private final int size;
+    private int size = 0;
     private final Scanner scanner = new Scanner(System.in);
     //C:\StudentWork\MiniProject\animal_resources
 
-    private Animal(String name, String imageFile, String soundFile, String[] words) {
+    private Animal(String name, int size, String imageFile, String soundFile) {
         this.name = name;
+        this.size = size;
         this.imageFile = imageFile;
         this.soundFile = soundFile;
-        this.size = size;
     }
 
-    public static Animal createInstance(String name, String imageFile, String soundFile, String[] words) {
-        return new Animal(name, imageFile, soundFile, words);
+    public static List<Animal> createAnimalFromFile(String filePath) {
+        List<Animal> animals = new ArrayList<>();
+        try (BufferedReader animalTextFile = new BufferedReader(new FileReader(animalConstructor))) {
+            String line;
+            while ((line = animalTextFile.readLine()) !=null) {
+                String[] data = line.split(",");
+                if (data.length == 4) {
+                    String name = data[0].trim();
+                    int size = Integer.parseInt(data[1].trim());
+                    String imageFile = data[2].trim();
+                    String soundFile = data[3].trim();
+                    Animal animal = new Animal(name, size, imageFile, soundFile);
+                    animals.add(animal);
+                } else {
+                    System.out.println("Invalid line/data" + line);
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return animals;
     }
 
     // if/else statement if array.length == 1 , animal is BLANK, if == 2, animal NEXT_BLANK,
@@ -43,7 +63,7 @@ public class Animal<Word> {
     // @TODO: set a file path, create local path and provide file
     // Reads image from text file
     public String displayImage() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(imageFilePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(imageFile))) {
 
             while ((imageFile = reader.readLine()) != null) {
                 return imageFile;
@@ -70,7 +90,7 @@ public class Animal<Word> {
 
         }
     }
-//    // @TODO: set a file path, create local path and provide file
+
 //    public String getSound() {
 //        return sound;
 //    }
@@ -81,6 +101,7 @@ public class Animal<Word> {
     }
 
     // After words have been pulled from text file. This will assign to animals
+    // TODO: change variables to reflect those above
     public String getWordCount() {
         List<String> animalWords = List.of(new String[wordsArray.length]); // needs to be given from Location enum
         int index = 0;
@@ -99,6 +120,7 @@ public class Animal<Word> {
     }
 
     // Reading from the file to get words
+    // TODO: change variables to reflect those above
     public String[] getWordFile() {
         List<String> words = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader("words.txt"))) {
@@ -116,6 +138,7 @@ public class Animal<Word> {
     }
 
     // This checks if the user input matches the word. Check Prompter class from Jay?
+    // TODO: change variables to reflect those above
     private String calculateHit() {
         int wordIndex = 0;
         int givenIndex = size; // @TODO this would be the number/size/wordCount from location enum, change accordingly
